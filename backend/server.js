bd3adb748f3bd5ea45572af079910d846f7d96e3
Mihir Biswas <biswasmihir355@gmail.com>
2024-11-06 20:38:54 +0530

    added path package

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
